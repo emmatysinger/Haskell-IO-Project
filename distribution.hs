267fc4880d541127ea5b3f844c3208fadd564217
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2019-03-19 10:17:10 -0400

    Update distribution.hs

--- distribution.hs
+++ distribution.hs
@@ -31,4 +31,38 @@
     * Letters that do not occur in the text are not listed in the output at all.
 -}
 
-main = putStrLn "Put your program here!"
+import Data.Char (toLower)
+
+count :: Eq a => a -> [a] -> Int
+count a xs = length [x | x <-xs, x==a]
+
+lengths :: [Char] -> [(Int,Char)]
+lengths z = filter p (zip [count a z|a <-['a'..'z']] ['a'..'z'])
+    where p x = fst x > 0
+
+ordertuples :: (Ord a1, Ord a) => (a1, a) -> (a1, a) -> Bool
+ordertuples a b
+    |fst a > fst b = True
+    |fst a < fst b = False
+    |fst a == fst b && snd a < snd b = True
+    |otherwise = False
+
+quicksort :: (Ord a1, Ord a) => [(a1, a)] -> [(a1, a)]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, ordertuples x a]
+        biggerSorted = quicksort [a | a <- xs, ordertuples a x]  
+    in  biggerSorted ++ [x] ++ smallerSorted  
+
+newlist :: [(Int, a)] -> [[a]]
+newlist xs = map p xs
+    where p x = replicate (fst x) (snd x)
+
+
+main = do
+    putStrLn "Please enter a string of text (the bigger the better): "
+    string <- getLine
+    let lower = map toLower string
+        new = (newlist.quicksort.lengths) lower
+    putStrLn $  "The distribution of characters in \"" ++ string ++ "\" is:"
+    mapM_ putStrLn new
